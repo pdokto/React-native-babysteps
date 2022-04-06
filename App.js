a65cc0d2e0d88cf1384249803a71184fb69b16cc
Patrick Doktorowski <patrick.doktorowski@googlemail.com>
2022-04-06 18:39:43 +0200

    first iteration of stopwatch

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState, useRef} from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import Timer from './components/Timer'
+
+
 
 export default function App() {
+  const [timer, setTimer] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+  const [isPaused, setIsPaused] =useState(false)
+  const increment = useRef(null)
+  
+  
+  const handleStart = () =>{
+    setIsRunning(true)
+    setIsPaused(false)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1)
+    }, 1000)
+  }
+  
+  const handlePause = () =>{
+    clearInterval(increment.current)
+    setIsPaused(true)
+    
+  }
+  
+  const handleResume = () =>{
+    setIsPaused(false)
+    increment.current = setInterval(() => {
+      setTimer((timer) => timer + 1)
+    }, 1000)
+  }
+  
+  const handleReset = () =>{
+    clearInterval(increment.current)
+    setIsRunning(false)
+    setIsPaused(false)
+    setTimer(0)
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello world! This is my first React Native app</Text>
+      <Text>React StopWatch</Text>
       <StatusBar style="auto" />
+      <Text>{timer}</Text>
+      
+          <Button onPress={handleStart} title="Start"/>
+          <Button onPress={handlePause} title="Stop"/>
+          <Button onPress={handleResume} title="Resume"/>
+          <Button onPress={handleReset} title="Reset"/>
+           
+      <Text>{increment.current}</Text>
     </View>
   );
 }
@@ -17,4 +62,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ components/Timer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Timer() {
+  return (
+    <View>
+        <Text>das ist timer</Text>
+    
+    </View>
+  )
+}
