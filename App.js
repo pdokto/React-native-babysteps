cd560951db0df175ae937ba10ff1528909a5921c
Patrick Doktorowski <patrick.doktorowski@googlemail.com>
2022-04-26 15:17:37 +0200

    added svg import

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { Component, useState, useRef} from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import Timer from './components/Timer'
+import Svg, { Circle } from 'react-native-svg'
+
 
 
 
@@ -44,14 +46,17 @@ export default function App() {
       <Text>React StopWatch</Text>
       <StatusBar style="auto" />
       <Text>{timer}</Text>
-      
-          <Button onPress={handleStart} title="Start"/>
-          <Button onPress={handlePause} title="Stop"/>
-          <Button onPress={handleResume} title="Resume"/>
+        <Button onPress={handleStart} title="Start"/>
+          {isRunning ? <Button onPress={handlePause} title="Stop"/> : <Button onPress={handleResume} title="Resume"/>}
+
+        
           <Button onPress={handleReset} title="Reset"/>
            
-      <Text>{increment.current}</Text>
-    </View>
+      
+      <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="purple" strokeWidth=".5" fill="violet" />
+      </Svg>
+  </View>
   );
 }
 
